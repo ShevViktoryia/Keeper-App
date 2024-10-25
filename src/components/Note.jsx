9aba552f703d69158a4e49500eda688fa2f8eaf3
Victoria <shevvictoryia@gmail.com>
2024-10-25 23:42:45 +0300

    feat: add Note component

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Note = () => {
-  return <div>Note</div>;
+  return (
+    <div>
+      <h3>This is the title</h3>
+      <p>This is the content</p>
+    </div>
+  );
 };
 
 export default Note;
